e16646fcc3958116dfb2cadd4ef4995e82bc8653
Liao Kangya Kay <kangyaliao@gmail.com>
2013-11-05 11:00:26 +0800

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by LKY on 5/11/13.
+//  Copyright (c) 2013 SCB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
